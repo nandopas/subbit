9af6a0d99bb84df2000eec17e08c53c7974abab3
nandopas <nandopas2@gmail.com>
2019-11-19 20:03:23 -0500

    ill fix this later, gonna go look at the way the post ids are done back on the master branch. so get rid of the link to a specific post in whatever view that was in. its breaking the app. also i dont remember which one. good luck future nando

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
 #    end
   end
 
-  root 'subway_stops#index'
-
-  get '/subway_stops/:subway_stop_id/posts', to: 'posts#index' 
-  
-  get 'posts/index'
+  #root 'subway_stops#index'
+  root 'home#index'
+  get '/posts', to: 'posts#index' 
+  get '/subway_stops/:subway_stop_id/posts', to: 'posts#show' 
+  #get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
