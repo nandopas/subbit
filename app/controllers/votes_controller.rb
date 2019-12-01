12c3c816b29b63e2015db6eb5f6d32675d9e7ccd
nandopas <nandopas2@gmail.com>
2019-12-01 18:36:07 -0500

    i messed up the voting system, gonna delte it and use a gem

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,18 @@
+class VotesController < ApplicationController
+  def create
+  	@vote - Vote.new(vote_params)
+  	@vote.post = Post.find(params[:post_id])
+  	if @vote.save
+  	  redirect_to @vote.post
+  	else
+  	  flash.now[:alert] = "Could not vote (expand on this and why"
+  	end
+  end
+
+
+  private
+    def vote_params
+      params.require(:vote).permit(:user_id, :post_id)
+    end
+
+end
---
+++ app/models/vote.rb
@@ -0,0 +1,7 @@
+class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :post, uniqueness { scope :post }
+  validates :user, uniqueness { scope :user }
+end
