8ab81216d7d0415f559b49de974195a17b3c71f9
nandopas <nandopas2@gmail.com>
2019-12-01 22:57:39 -0500

    deleted old votes table

---
+++ db/migrate/20191202035112_drop_table_votes.rb
@@ -0,0 +1,5 @@
+class DropTableVotes < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :votes
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_164042) do
+ActiveRecord::Schema.define(version: 2019_12_02_035112) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
