7ee1ee071344efe3a2a860205415ba72a9f24ec6
nandopas <nandopas2@gmail.com>
2020-02-01 10:58:03 -0500

    added a semi colon to uniqueness symbol (syntax error maybe

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :post, uniqueness { scope :post }
-  validates :user, uniqueness { scope :user }
+  validates :post, uniqueness: { scope :post }
+  validates :user, uniqueness: { scope :user }
 end
