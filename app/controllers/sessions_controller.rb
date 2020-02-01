30122455c949670640750b70401bf8a49a24f754
nandopas <nandopas2@gmail.com>
2020-02-01 11:26:22 -0500

    sign in by username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
 
   def create
 
-    user = User.find_by_email(params[:email])
+    user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
+      flash.now[:alert] = "Username or password is invalid"
       render "new"
     end
 
