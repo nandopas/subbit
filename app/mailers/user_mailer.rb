a68bef4bf12c1527508f58433f8025d2973a7cad
nandopas <nandopas2@gmail.com>
2020-02-07 20:19:13 -0500

    i like this to deploy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     #@url  = 'http://example.com/login'
-    mail(to: [@user.email, 'nandopas2@gmail.com'], subject: 'Welcome to Subbit!')
+    mail(to: [@user.email], subject: 'Welcome to Subbit!')
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   acts_as_votable
   belongs_to :subway_stop
   belongs_to :user
+  validates :topic, presence: true,
+                    length: { minimum: 1, maximum: 140 }
 end
