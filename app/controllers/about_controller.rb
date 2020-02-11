826f119c2346af0ab980ecea5da5767cf1f661f1
nandopas <nandopas2@gmail.com>
2020-02-10 23:08:33 -0500

    created instance variables in about controller to query for the correct subway stops instead of placing ids since postgre runs differently'
    '

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,8 @@
 class AboutController < ApplicationController
   def index
+  	@columbia = SubwayStop.find_by stop: "116 St - Columbia University"
+  	@nyu = SubwayStop.find_by stop: "W 4 St - NYU"
+  	@soho = SubwayStop.find_by stop: "Prince St"
+  	@us = SubwayStop.find_by stop: "14 St - Union Sq"
   end
 end
