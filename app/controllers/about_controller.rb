875d6ce605494e45dee34dfcd1aaf96197cc34c6
nandopas <nandopas2@gmail.com>
2020-02-17 17:54:42 -0500

    spelling error

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,7 @@
 class AboutController < ApplicationController
   def index
   	@columbia = SubwayStop.find_by stop: "116 St - Columbia University"
-  	@nyu = SubwayStop.find_by stop: "W 4 St - NYU/Washington Sqaure Park"
+  	@nyu = SubwayStop.find_by stop: "W 4 St - NYU/Washington Square Park"
   	@soho = SubwayStop.find_by stop: "Prince St"
   	@us = SubwayStop.find_by stop: "14 St - Union Sq"
   end
