67ed2f78a9f3ed3d09b0fa2e03f682ce28700f98
nandopas <nandopas2@gmail.com>
2020-05-27 23:05:56 -0400

    changed files to try deploy again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://subbit.net/', 'https://subbit-nyc.herokuapp.com/'
   
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+  
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_subbit', domain: 'your-frontend-domain'
+  Rails.application.config.session_store :cookie_store, key: '_subbit', domain: 'https://subbit.net/'
 else
   Rails.application.config.session_store :cookie_store, key: '_subbit' 
 end
